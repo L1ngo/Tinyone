fd030e46dc26dcb7a6f77c2dc157597f0137a4e5
Andrey <blurlingo@gmail.com>
2020-01-17 16:45:49 +0200

    Tinyone v1.0

---
+++ js/script.js
@@ -0,0 +1,26 @@
+let doc = document;
+
+let ready = (callback) => {
+    if (doc.readyState != 'loading') callback();
+    else doc.addEventListener('DOMContentLoaded', callback);
+};
+
+ready(() => {
+    openNav();
+});
+
+
+function openNav() {
+    let burger          = doc.querySelector('.header-burger');
+    let nav             = doc.querySelector('.header-nav');
+    let checkScrollLock = false;
+    let fader           = doc.querySelector('.fader');
+
+    if (burger) {
+        burger.addEventListener('click', function () {
+            burger.classList.toggle('header-burger_open');
+            nav.classList.toggle('header-nav_open');
+            fader.classList.toggle('fader_visible');
+        });
+    }
+}
